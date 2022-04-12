2f078fed133f09a655f504c9a1dd19bc5736521f
PjyothiK <jyothi.palli30@gmail.com>
2022-04-12 11:29:16 +0530

    added README.md

---
+++ src/app/counter/custom-counter-input/custom-counter-input.component.ts
@@ -0,0 +1,30 @@
+import { Observable } from 'rxjs';
+import { getChannelName } from './../state/counter.selectors';
+import { changeChannelName, customIncrement } from './../state/counter.actions';
+import { CounterState } from './../state/counter.state';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+@Component({
+  selector: 'app-custom-counter-input',
+  templateUrl: './custom-counter-input.component.html',
+  styleUrls: ['./custom-counter-input.component.css'],
+})
+export class CustomCounterInputComponent implements OnInit {
+  value: number;
+  channelName$: Observable<string>;
+
+  constructor(private store: Store<{ counter: CounterState }>) {}
+
+  ngOnInit(): void {
+    this.channelName$ = this.store.select(getChannelName);
+  }
+
+  onAdd() {
+    this.store.dispatch(customIncrement({ count: +this.value }));
+  }
+
+  onChangeChannelName() {
+    this.store.dispatch(changeChannelName());
+  }
+}
